57df5ef79e152e34c84f006fe39193b236d911d4
Orky63 <131435557+Orky63@users.noreply.github.com>
2024-07-30 11:07:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+//const min = 50;
+//const max = 100;
+
+//let randomNum = Math.floor(Math.random() * (max-min)) + min;
+
+//console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}
